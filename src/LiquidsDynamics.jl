ec29649db6e5523c686c8a1f0b57cae8eb63ee0f
Pablo Zubieta <pabloferz@yahoo.com.mx>
2019-08-16 13:30:00 -0500

    Upload dynamics code without auxiliar modules

--- src/LiquidsDynamics.jl
+++ src/LiquidsDynamics.jl
@@ -1,5 +1,40 @@
 module LiquidsDynamics
 
-greet() = print("Hello World!")
+
+### Imports
+using Reexport
+using Parameters
+using Projections
+@reexport using LiquidsStructureGrids
+using LinearAlgebra: I
+
+
+### Exports
+export dynamics, dynamics!, initialize_dynamics
+
+
+### Implementation
+include("utils.jl")
+include("initialization.jl")
+include("globaldynamics.jl")
+include("shorttimes.jl")
+include("intermediatetimes.jl")
+
+
+function __init__()
+    # Hack to work around some performance issues in base (#32552, #28683)
+    # Run a method and eval a part of the function that uses dynamic dispatch
+
+    structure = StructureFactor(DipolarHardSpheres(0.3, 0.05), MSA{VerletWeis})
+    grid = ChebyshevGrid(0, 50, 2^9)
+    S = StructureFactorGrid(structure, grid)
+    dynamics(S, 7.2, 1e6, n = 16)
+
+    @eval Projections begin
+        *(v::TR, p::MDProjections{2}) =
+            (@inbounds TR(v.t * (p.t + p.r[1]), v.r * p.r[2]))
+    end
+end
+
 
 end # module
